c963cac6dc8f72b348ce124c20245e45ea606691
unknown <chaukajal072004@gmail.com>
2025-02-10 16:14:18 +0530

    fix1 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home"; 
+import Home from "./Pages/Home"; 
 import Navbar from "./components/Navbar"
 import ProductDetails from "./pages/ProductDetails"
 
